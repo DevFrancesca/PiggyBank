90ce7c38f8993a3eac99a2fbf4ff783e1d379f55
DevFranceca <dynamicfranchescer@gmail.com>
2024-08-26 16:04:45 +0100

    added texts to view

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>This is Home screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
